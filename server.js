fb62be4bddbc294593b966830bc8f0d2900d89c3
yu <Amily472@outlook.com>
2025-02-26 20:39:04 +0800

    启用 CORS

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin: '*', // 允许所有域访问
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type']
+    }
+));
 
 // 连接 MongoDB
 mongoose.connect(process.env.MONGO_URI)
